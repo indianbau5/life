014ba521febd70260a00c4533b52ea2adaaed312
indianbau5 <siva.somayyajula@gmail.com>
2014-02-16 19:05:00 -0500

    red vs. blue (thanks Patrick)

--- main.lua
+++ main.lua
@@ -1,19 +1,12 @@
---[[
-    life, by Siva Somayyajula
-             Patrick Do
-             Shrikant Mishra
-             Peter Zhao
-]]--
-
+-- life and war -- Siva Somayyajula & Patrick Do
 state   = require 'hump.gamestate'
 class   = require 'hump.class'
 signal  = require 'hump.signal'
 --message = require 'messages.MessageInABottle'
+--import signals to dispatch events during gameplay
+--use gui for deployments
 
---IDEA: Use signals to check if user lost/won and same for AI?
---gui 'quickie' will be used for deployments
-
-DEAD, ALIVE = 0, 1
+DEAD, BLUE, RED = 0, 1, 2
 
 local width, height = love.graphics.getDimensions()
 if width < height then
@@ -22,24 +15,15 @@ else
 	SIZE = height
 end
 
-SPACE        = 20
-N            = SIZE / SPACE
-BLACK, BLUE,
-       WHITE = {49, 79, 79}, {100, 149, 237},
-	           {255, 255, 255}
+SPACE = 20
+N     = SIZE / SPACE
 
-GAMESPEED	 = 5
+CBLACK, CBLUE,
+CWHITE, CRED   = {49, 79, 79}, {100, 149, 237},
+                 {255, 255, 255}, {205, 92, 92}
+
+GAMESPEED    = 5
 score        = 0
-
-player = class {
-	init = function(self)
-	end
-}
-
-AI = class {
-	init = function(self)
-	end
-}
 
 level = class {
 	init = function(self)
@@ -58,7 +42,7 @@ level = class {
 }
 
 function level:enter(previous)
-	love.graphics.setBackgroundColor(unpack(BLACK))
+	love.graphics.setBackgroundColor(unpack(CBLACK))
 	love.graphics.clear()
 end
 
@@ -89,7 +73,7 @@ function level:update(dt)
 			end
 		end
 	end
-	function cell(x, y)
+	function get(x, y)
 		if x < 1 then
 			x = N
 		elseif x > N then
@@ -105,20 +89,32 @@ function level:update(dt)
 	clear()
 	for x = 1, N do
 		for y = 1, N do
-			local neighbors = 0
+			local neighbors = {}
 			for i = -1, 1 do
 				for j = -1, 1 do
 					if not (i == 0 and j == 0) then
-						neighbors = neighbors + cell(x + i, y + j)
+						neighbors[table.maxn(neighbors) + 1] = get(x + i, y + j)
 					end
 				end
 			end
-			if self.grid[x][y] == ALIVE and
-			  (neighbors < 2            or
-			   neighbors > 3)           then
+			local sumBlue, sumRed = 0, 0
+			for sum = 1, table.maxn(neighbors) do
+				if neighbors[sum] == BLUE then
+					sumBlue = sumBlue + 1
+				elseif neighbors[sum] == RED then
+					sumRed  = sumRed  + 1
+				end
+			end
+			if self.grid[x][y] ~= DEAD and
+			  (sumBlue + sumRed < 2     or
+			   sumBlue + sumRed > 3)    then
 				self.buffer[x][y] = DEAD
-			elseif neighbors == 3 then
-				self.buffer[x][y] = ALIVE
+			elseif sumBlue + sumRed == 3 and self.grid[x][y] == DEAD then
+				if sumBlue > 1 then
+					self.buffer[x][y] = BLUE
+				elseif sumRed > 1 then
+					self.buffer[x][y] = RED
+				end
 			end
 		end
 	end
@@ -131,16 +127,18 @@ function level:draw()
 	end
 	for x = 1, N do
 		for y = 1, N do
-			if self.grid[x][y] == ALIVE then
-				love.graphics.setColor(unpack(BLUE))
+			if self.grid[x][y] == BLUE then
+				love.graphics.setColor(unpack(CBLUE))
+			elseif self.grid[x][y] == RED then
+				love.graphics.setColor(unpack(CRED))
 			else
-				love.graphics.setColor(unpack(BLACK))
+				love.graphics.setColor(unpack(CBLACK))
 			end
 			love.graphics.rectangle("fill",
 				toPixel(y), toPixel(x), SPACE, SPACE)
 		end
 	end
-	love.graphics.setColor(unpack(WHITE))
+	love.graphics.setColor(unpack(CWHITE))
 	for x = 0, SIZE, SPACE do
 		love.graphics.line(x, 0, x, SIZE)
 	end
@@ -165,11 +163,15 @@ function level:mousereleased(x, y, button)
 	function toCell(p)
 		return math.floor((p + 1) / SPACE + 1)
 	end
-	if button == 'l' then
-		local yy, xx = toCell(x), toCell(y)
-		if yy <= N and xx <= N and
-		   yy >= 1 and xx >= 1 then
-			self.grid[xx][yy] = ALIVE - self.grid[xx][yy]
+	local yy, xx = toCell(x), toCell(y)
+	if yy <= N and xx <= N and
+	   yy >= 1 and xx >= 1 then
+		if self.grid[xx][yy] ~= DEAD then
+			self.grid[xx][yy] = DEAD
+		elseif button == 'l' then
+			self.grid[xx][yy] = BLUE
+		else
+			self.grid[xx][yy] = RED
 		end
 	end
 end
